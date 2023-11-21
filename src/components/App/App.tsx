e95cc0079cd69e93d924d53d67d6bc9bba57283b
Nikolay Kalugin <Nikolay.Kalugin@x5.ru>
2023-11-21 04:18:50 +0300

    started making header

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+
+import '../App/App.css';
+import AppHeader from '../AppHeader/app-header';
+
+function App() {
+  return (
+    <div className="App">
+      <AppHeader/>
+      {/* <BurgerIngredients/> */}
+      {/* <BurgerConstructor/> */}
+    </div>
+  );
+}
+
+export default App;

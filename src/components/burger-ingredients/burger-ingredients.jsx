112332e318c2575a94fc2f3e99c301871b0ec01a
Nikolay Kalugin <Nikolay.Kalugin@x5.ru>
2023-11-27 20:29:56 +0300

    create burger ingridients

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { Tab, ConstructorElement } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Tab, ConstructorElement, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './burger-ingredients.module.css';
 
 
@@ -17,13 +17,13 @@ function BurgerIngredients(props)
 			<h1 className={styles.MainTitle}>Соберите бургер</h1>
 
 			<div className={styles._Tabs}>
-				<Tab key="1" value="one" active={current_tab === 'one'} onClick={setCurrent}>
+				<Tab value="one" active={current_tab === 'one'} onClick={setCurrent}>
 					Булки
 				</Tab>
-				<Tab key="2" value="two" active={current_tab === 'two'} onClick={setCurrent}>
+				<Tab value="two" active={current_tab === 'two'} onClick={setCurrent}>
 					Соусы
 				</Tab>
-				<Tab key="3" value="three" active={current_tab === 'three'} onClick={setCurrent}>
+				<Tab value="three" active={current_tab === 'three'} onClick={setCurrent}>
 					Начинки
 				</Tab>
 			</div>
@@ -31,7 +31,7 @@ function BurgerIngredients(props)
 			<div>
 				<h2>Бургеры</h2>
 
-				<ul className={styles.IngredientsList}> 
+				<ul className={styles.IngredientsList} custom-scroll > 
 					{
 						ingredients.length !==0 && ingredients.map( ingredient => 
 							<li key={ingredient._id}>
@@ -39,8 +39,14 @@ function BurgerIngredients(props)
 									text={ingredient.name}
 									price={ingredient.price}
 									thumbnail={ingredient.image}
-									isLocked={true}
-								/>
+									isLocked={undefined}>
+
+	 								<Counter count={1} size="default" extraClass="m-1" />
+
+								</ConstructorElement>
+									
+							
+								
 							</li>
 						)
 					}
